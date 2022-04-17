a73d2aaa1a33f338ad83920abed562b655b56e10
mollikacomputer <mollikacomputer3@gmail.com>
2022-04-17 17:59:46 +0600

    service bootstrap card added

--- src/pages/Service/Service.js
+++ src/pages/Service/Service.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import './Service.css'
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import "./Service.css";
 const Service = (props) => {
-    const {title, price, description,picture} = props.service;
-    return (
-        <div className='my-5'>
-            <img className='img-fluid' src={picture} alt="" />
-            <h2> {title} </h2>
-            <h5> {description}  </h5>
-        </div>
-    );
+  const { title, price, description, picture } = props.service;
+  return (
+    <Card className="my-5" style={{ width: "22rem" }}>
+      <Card.Img variant="top" src={picture} />
+      <Card.Body>
+        <Card.Title> Honorable Amount {price} </Card.Title>
+        <Card.Title> {title} </Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Button variant="primary"> Book Now </Button>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Service;
--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -11,7 +11,7 @@ const Services = () => {
     return (
         <div className='container'>
             <h2> Service Page</h2>
-            <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div className="row row-cols-1 row-cols-md-3">
                 {
                     services.map(service => <Service
                     key={service.id}
