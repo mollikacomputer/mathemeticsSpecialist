ccd925057e2faaa73f3e09ef7de6d47a39805943
mollikacomputer <mollikacomputer3@gmail.com>
2022-04-17 21:57:21 +0600

    google signin done

--- src/pages/LogIn/Login.js
+++ src/pages/LogIn/Login.js
@@ -1,7 +1,37 @@
-import React from "react";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import app from "../../firebase.init";
 
+const auth = getAuth(app);
 const Login = () => {
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleGoogleSignIn = () =>{
+    const googleProvider = new GoogleAuthProvider();
+    signInWithPopup(auth, googleProvider)
+    .then(result => {
+      const user = result.user;
+      setUser(user.displayName);
+      setEmail(user.email);
+      // console.log(user.displayName, user.email);
+      
+    })
+    .catch(error=>{
+      console.error(error);
+    })
+  };
+  const handleSignOut = () =>{
+    const auth = getAuth();
+    signOut(auth)
+    .then(()=>{
+      setUser({});
+    })
+    .catch(error => {
+      setUser({});
+    })
+  }
   return (
     <div className='my-5 py-3 w-50 mx-auto'>
       <h2 className="text-primary" > Please Log in</h2>
@@ -16,9 +46,19 @@ const Login = () => {
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
         <Button variant="primary w-100" type="submit">
-          Submit
+         Log in
         </Button>
       </Form>
+      <br/>
+      {
+        user ? <button onClick={handleSignOut} className="btn btn-primary" > SignOut </button>
+        :
+        <button onClick={handleGoogleSignIn} className="btn btn-primary" > Google Sign In </button>
+      }<br/>
+      <h2 className="text-primary"> {user} </h2>
+      
+      {/* <button onClick={handleSignOut} className="btn btn-primary" > SignOut </button> */}
+      
     </div>
   );
 };
