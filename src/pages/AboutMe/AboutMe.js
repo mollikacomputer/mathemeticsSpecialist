805a0433ad31df79b6c1544ded8f2b5357c63ee6
mollikacomputer <mollikacomputer3@gmail.com>
2022-04-18 20:46:55 +0600

    firebase blog section added

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -4,6 +4,7 @@ const AboutMe = () => {
     return (
         <div className='my-5'>
             <h2> I am Ranjit K. Mandal</h2>
+            <p>I'm Ranjit Kumar Mandal. I'm a small business owner. I want to increase my income with freelancing or some own small webdevelopment company for local country or international company, I'm trying to develop my skill. End of this year 2022 around septembor I could add some extra income from my webdevelopment knowledge</p>
         </div>
     );
 };
